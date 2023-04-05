fa40ed7ff94354b9732e60da75a91f5a6375a29e
yogioctalogic <shreyog@octalogic.in>
2023-04-05 17:25:41 +0530

    Changelog - individual blog page update

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,13 @@
 import { GetStaticPropsContext } from "next";
 import * as React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Link } from "@mui/material";
+import { format, parseISO } from "date-fns";
+import {
+  FiberManualRecord as FiberManualRecordIcon,
+  Facebook as FacebookIcon,
+  Twitter as TwitterIcon,
+  LinkedIn as LinkedInIcon,
+} from "@mui/icons-material";
 
 // import { useTina } from "tinacms/dist/react";
 
@@ -21,7 +28,55 @@ export default function Home(props: IPostProps) {
 
   return (
     <Grid className="global-spacer">
-      <Typography sx={{ textAlign: "center", fontWeight: 600, fontSize: "1.2rem" }}>You may also like</Typography>
+      <Typography sx={{ paddingBlockStart: "4rem" }}>
+        {`Updated ${format(new Date(), "MMM dd, yyyy")}`}
+        {/* {postData ? format(parseISO(postData?.publishDate), 'MMM dd, yyyy') : ''} */}
+      </Typography>
+
+      <Grid
+        container
+        sx={{
+          paddingBlock: "1rem",
+          flexDirection: { xs: "column", sm: "row" },
+        }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Typography component="span">Written by</Typography>
+
+          {/* <FiberManualRecordIcon /> */}
+
+          {/* <Typography component="span">Reviewed by</Typography> */}
+        </Grid>
+
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          sx={{
+            justifyContent: { xs: "flex-start", sm: "flex-end" },
+            gap: "0.5rem",
+          }}
+          alignItems={"center"}
+        >
+          <Typography>Share</Typography>
+          <Link target="_blank">
+            <FacebookIcon />
+          </Link>
+          <Link target="_blank">
+            <TwitterIcon />
+          </Link>
+          <Link>
+            <LinkedInIcon />
+          </Link>
+        </Grid>
+      </Grid>
+
+      <Typography
+        sx={{ textAlign: "center", fontWeight: 600, fontSize: "1.2rem" }}
+      >
+        You may also like
+      </Typography>
       <Grid
         sx={{
           marginBlockStart: "2rem",
