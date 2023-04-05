19c6b4b5d00a9266bbda6e59caf5f3068430ea2b
yogioctalogic <shreyog@octalogic.in>
2023-04-05 13:04:17 +0530

    Changelog - blog listing page

---
+++ src/constants/posts.ts
@@ -0,0 +1,51 @@
+export type PostObjType = {
+  topic: string;
+  title: string;
+  summary: string;
+  tags?: string[];
+  url?: string;
+};
+
+export const POSTS = [
+  {
+    topic: "Web Development",
+    title: "Tired of slow website speeds? Meet CDN! 💻",
+    summary:
+      "Have you ever visited a website that took forever to load? We've all been there, and it's frustrating. In today's fast-paced world, people expect websites to load quickly, and if they don't, they're likely to bounce to a competitor's site. This is where Content Delivery Networks (CDNs) come in. CDNs are a network of servers located around the world that work together to deliver content to users quickly and efficiently. In this blog post, we will take a closer look at CDNs, how they work, and how they can benefit your website. ",
+    tags: ["Cloud"],
+    url: "",
+  },
+  {
+    topic: "Dev Ops",
+    title: "Docker: Simplifying Application Deployment Like a Pro 🐳 ",
+    summary:
+      "Docker is a containerization platform that allows developers to package their applications and dependencies into portable, self-contained units, called containers. These containers can then be deployed across different environments, without the need for complex setup or configuration. In this blog post, we will take a closer look at Docker, how it works, and its benefits for developers and organizations. So, get ready to unleash the power of Docker and simplify your application deployment process! 🚀",
+    tags: ["Dev Ops", "Cloud"],
+    url: "",
+  },
+  {
+    topic: "Dev Ops",
+    title: "Docker: Simplifying Application Deployment Like a Pro 🐳 ",
+    summary:
+      "Docker is a containerization platform that allows developers to package their applications and dependencies into portable, self-contained units, called containers. These containers can then be deployed across different environments, without the need for complex setup or configuration. In this blog post, we will take a closer look at Docker, how it works, and its benefits for developers and organizations. So, get ready to unleash the power of Docker and simplify your application deployment process! 🚀",
+    tags: ["Dev Ops", "Cloud"],
+    url: "",
+  },
+  {
+    topic: "Web Development",
+    title: "Tired of slow website speeds? Meet CDN! 💻",
+    summary:
+      "Have you ever visited a website that took forever to load? We've all been there, and it's frustrating. In today's fast-paced world, people expect websites to load quickly, and if they don't, they're likely to bounce to a competitor's site. This is where Content Delivery Networks (CDNs) come in. CDNs are a network of servers located around the world that work together to deliver content to users quickly and efficiently. In this blog post, we will take a closer look at CDNs, how they work, and how they can benefit your website. ",
+    tags: ["Cloud"],
+    url: "",
+  },
+];
+
+export const HERO_POST = {
+  topic: "Web Development",
+  title: "Say goodbye to servers and hello to serverless architecture! 🚀",
+  summary:
+    "In recent years, serverless computing has emerged as a popular paradigm for developing and deploying software applications. Despite the name, serverless does not mean that there are no servers involved in the process; rather, it refers to the fact that developers no longer have to worry about the underlying infrastructure and can focus solely on ",
+  tags: ["Serverless", "Solution Architecture"],
+  url: "",
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
 import { Typography, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -12,7 +9,17 @@ import client from "../../tina/__generated__/client";
 
 import IPostListProps from "@/interfaces/IPostListProps";
 
-const TOPIC_TEXT_VARIANT_OPTIONS = {
+import { PostObjType, POSTS, HERO_POST } from "@/constants/posts";
+
+type CustomTypographyProps = {
+  size: string;
+};
+
+type CustomButtonProps = {
+  customSize: string;
+};
+
+const BLOG_CARD_SIZE_OPTIONS = {
   large: "large",
   medium: "medium",
 };
@@ -33,18 +40,13 @@ const TOPIC_TEXT_VARIANTS: {
   },
 };
 
-const TopicText = styled(Typography)((props) => ({
-  ...(props.customVariant
-    ? TOPIC_TEXT_VARIANTS[props.customVariant as keyof {}]
-    : TOPIC_TEXT_VARIANTS[TOPIC_TEXT_VARIANT_OPTIONS.medium as keyof {}]),
+const TopicText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? TOPIC_TEXT_VARIANTS[props.size as keyof {}]
+    : TOPIC_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
   color: "#999999",
   fontWeight: 700,
 }));
-
-const BLOG_HEADING_TEXT_VARIANT_OPTIONS = {
-  large: "large",
-  medium: "medium",
-};
 
 const BLOG_HEADING_TEXT_VARIANTS: {
   [key: string]: {
@@ -62,19 +64,12 @@ const BLOG_HEADING_TEXT_VARIANTS: {
   },
 };
 
-const BlogHeadingText = styled(Typography)((props) => ({
-  ...(props.customVariant
-    ? BLOG_HEADING_TEXT_VARIANTS[props.customVariant as keyof {}]
-    : BLOG_HEADING_TEXT_VARIANTS[
-        BLOG_HEADING_TEXT_VARIANT_OPTIONS.medium as keyof {}
-      ]),
+const BlogHeadingText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? BLOG_HEADING_TEXT_VARIANTS[props.size as keyof {}]
+    : BLOG_HEADING_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
   fontWeight: 700,
 }));
-
-const BLOG_SUMMARY_TEXT_VARIANT_OPTIONS = {
-  large: "large",
-  medium: "medium",
-};
 
 const BLOG_SUMMARY_TEXT_VARIANTS: {
   [key: string]: {
@@ -92,19 +87,12 @@ const BLOG_SUMMARY_TEXT_VARIANTS: {
   },
 };
 
-const BlogSummaryText = styled(Typography)((props) => ({
-  ...(props.customVariant
-    ? BLOG_SUMMARY_TEXT_VARIANTS[props.customVariant as keyof {}]
-    : BLOG_SUMMARY_TEXT_VARIANTS[
-        BLOG_SUMMARY_TEXT_VARIANT_OPTIONS.medium as keyof {}
-      ]),
+const BlogSummaryText = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? BLOG_SUMMARY_TEXT_VARIANTS[props.size as keyof {}]
+    : BLOG_SUMMARY_TEXT_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
   color: "#6B7280",
 }));
-
-const TAG_VARIANT_OPTIONS = {
-  large: "large",
-  medium: "medium",
-};
 
 const TAG_VARIANTS: {
   [key: string]: {
@@ -122,21 +110,14 @@ const TAG_VARIANTS: {
   },
 };
 
-const Tag = styled(Typography)((props) => ({
-  ...(props.customVariant
-    ? BLOG_SUMMARY_TEXT_VARIANTS[props.customVariant as keyof {}]
-    : BLOG_SUMMARY_TEXT_VARIANTS[
-        BLOG_SUMMARY_TEXT_VARIANT_OPTIONS.medium as keyof {}
-      ]),
+const Tag = styled(Typography)<CustomTypographyProps>((props) => ({
+  ...(props.size
+    ? TAG_VARIANTS[props.size as keyof {}]
+    : TAG_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
   backgroundColor: "#D1D5DB",
   color: "#6B7280",
   padding: "0.625rem 1.875rem",
 }));
-
-const READ_MORE_BTN_VARIANT_OPTIONS = {
-  large: "large",
-  medium: "medium",
-};
 
 const READ_MORE_BTN_VARIANTS: {
   [key: string]: {
@@ -154,21 +135,19 @@ const READ_MORE_BTN_VARIANTS: {
   },
 };
 
-const ReadMoreBtn = styled(Button)((props) => ({
-  ...(props.customVariant
-    ? READ_MORE_BTN_VARIANTS[props.customVariant as keyof {}]
-    : READ_MORE_BTN_VARIANTS[READ_MORE_BTN_VARIANT_OPTIONS.medium as keyof {}]),
+const ReadMoreBtn = styled(Button)<CustomButtonProps>((props) => ({
+  ...(props.customSize
+    ? READ_MORE_BTN_VARIANTS[props.customSize as keyof {}]
+    : READ_MORE_BTN_VARIANTS[BLOG_CARD_SIZE_OPTIONS.medium as keyof {}]),
   color: "#6B7280",
   textDecoration: "underline",
 }));
 
-const Tags = ({ tags, customVariant }: any) => {
+const Tags = ({ tags, size }: any) => {
   return (
     <Grid container alignItems="center" sx={{ gap: "0.5rem" }}>
       {React.Children.toArray(
-        tags.map((tag: string) => (
-          <Tag customVariant={customVariant}>{tag}</Tag>
-        ))
+        tags.map((tag: string) => <Tag size={size}>{tag}</Tag>)
       )}
     </Grid>
   );
@@ -177,6 +156,104 @@ const Tags = ({ tags, customVariant }: any) => {
 const truncate = ({ words = "", maxlength = 450 }) => {
   return `${words.slice(0, maxlength)} …`;
 };
+
+const BlogPostCard = ({ post, size }: { post: PostObjType; size: string }) => (
+  <Grid>
+    <Grid item>
+      <TopicText size={size}>{post.topic}</TopicText>
+    </Grid>
+    <Grid item sx={{ marginTop: "0.6rem" }}>
+      <BlogHeadingText size={size}>{post.title}</BlogHeadingText>
+    </Grid>
+    <Grid item sx={{ marginTop: "2rem" }}>
+      <BlogSummaryText size={size}>
+        {truncate({
+          words: post.summary,
+        })}
+      </BlogSummaryText>
+    </Grid>
+    <Grid item sx={{ marginTop: { xs: "1rem", sm: "1.5rem" } }}>
+      <Grid
+        container
+        sx={{
+          flexDirection: { xs: "column-reverse", sm: "row" },
+          justifyContent: { xs: "flex-start", sm: "space-between" },
+          gap: { xs: "1rem", sm: "0" },
+        }}
+        alignItems={"flex-start"}
+      >
+        <Grid item xs={12} sm={9}>
+          {Tags({
+            tags: post.tags,
+            size: size,
+          })}
+        </Grid>
+        <Grid
+          container
+          sx={{
+            justifyContent: { xs: "flex-start", sm: "flex-end" },
+          }}
+          item
+          xs={12}
+          sm={3}
+        >
+          <ReadMoreBtn customSize={size}>Read More</ReadMoreBtn>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>
+);
+
+const AboutOctalogicCard = () => (
+  <Grid
+    sx={{
+      padding: "1.5rem",
+      backgroundColor: "#3A9086",
+    }}
+  >
+    <Typography
+      sx={{
+        fontWeight: 700,
+        fontSize: "1.375rem",
+        lineHeight: "1.5rem",
+        color: "#F3F4F6",
+      }}
+    >
+      About
+    </Typography>
+
+    <Typography
+      sx={{
+        fontSize: "1.125rem",
+        lineHeight: "1.125rem",
+        color: "#F3F4F6",
+        marginTop: "1rem",
+      }}
+    >
+      Octalogic Tech is a SaaS company based in Goa, India, that provides custom
+      software development, mobile app development, and IT consulting services
+      to clients worldwide. Our turnkey solutions are designed to simplify the
+      process of software development and deployment, providing clients with a
+      seamless end-to-end experience.
+    </Typography>
+
+    <Typography
+      sx={{
+        fontSize: "1.125rem",
+        lineHeight: "1.125rem",
+        color: "#F3F4F6",
+        marginTop: "1rem",
+      }}
+    >
+      Being sector agnostic, we have provided tailor-made solutions to clients
+      in FinTech, MedTech, EdTech, FoodTech, and e-commerce verticals.With a
+      proven track record of delivering quality and innovative solutions to
+      clients in various industries, our team of experienced professionals
+      leverages cutting-edge technology and industry best practices that meet
+      client&apos;s specific needs and goals.
+    </Typography>
+  </Grid>
+);
 
 const InteractiveList = (props: IPostListProps) => {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -188,64 +265,38 @@ const InteractiveList = (props: IPostListProps) => {
   const postsList = data?.postConnection?.edges || [];
 
   return (
-    <Grid container sx={{ paddingX: "200px" }}>
-      <Grid>
-        <Grid item>
-          <TopicText customVariant={TOPIC_TEXT_VARIANT_OPTIONS.large}>
-            Web Development
-          </TopicText>
-        </Grid>
-        <Grid item sx={{ marginTop: "0.6rem" }}>
-          <BlogHeadingText
-            customVariant={BLOG_HEADING_TEXT_VARIANT_OPTIONS.large}
-          >
-            Say goodbye to servers and hello to serverless architecture! 🚀
-          </BlogHeadingText>
-        </Grid>
-        <Grid item sx={{ marginTop: "2rem" }}>
-          <BlogSummaryText
-            customVariant={BLOG_SUMMARY_TEXT_VARIANT_OPTIONS.large}
-          >
-            {truncate({
-              words: ` In recent years, serverless computing has emerged as a popular
-            paradigm for developing and deploying software applications. Despite
-            the name, serverless does not mean that there are no servers
-            involved in the process; rather, it refers to the fact that
-            developers no longer have to worry about the underlying
-            infrastructure and can focus solely on`,
-            })}
-          </BlogSummaryText>
-        </Grid>
-        <Grid item sx={{ marginTop: "1.5rem" }}>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems={"flex-start"}
-            // sx={{ gap: "1rem" }}
-          >
-            <Grid item xs={9}>
-              {Tags({
-                tags: ["Serverless", "Solution Architecture"],
-                customVariant: TAG_VARIANT_OPTIONS.large,
-              })}
-            </Grid>
-            <Grid container justifyContent={"flex-end"} item xs={3}>
-              <ReadMoreBtn customVariant={READ_MORE_BTN_VARIANT_OPTIONS.large}>
-                Read More
-              </ReadMoreBtn>
-            </Grid>
-          </Grid>
-        </Grid>
+    <Grid
+      container
+      sx={{ paddingX: { xs: "4rem", sm: "9rem", xl: "12.5rem" } }}
+    >
+      <Grid
+        sx={{
+          paddingBlockStart: "6rem",
+          display: "grid",
+          gridTemplateColumns: { xs: "1fr", lg: "2fr 1fr" },
+          gridRowGap: { xs: "5rem", lg: "7rem" },
+          gridColumnGap: { xs: "0", lg: "7rem" },
+        }}
+      >
+        <BlogPostCard post={HERO_POST} size={BLOG_CARD_SIZE_OPTIONS.large} />
+        <AboutOctalogicCard />
       </Grid>
-      {/* <Grid item xs={12} md={6}>
-        <List>
-          {postsList.map((post: any) => (
-            <ListItem key={post.node.id}>
-              <ListItemText primary={post.node._sys.filename} />
-            </ListItem>
-          ))}
-        </List>
-      </Grid> */}
+
+      <Grid
+        sx={{
+          paddingBlock: "8rem",
+          display: "grid",
+          gridTemplateColumns: { xs: "1fr", lg: "repeat(2, 1fr)" },
+          gridRowGap: { xs: "5rem", lg: "7rem" },
+          gridColumnGap: { xs: "0", lg: "7rem" },
+        }}
+      >
+        {React.Children.toArray(
+          POSTS.map((post) => (
+            <BlogPostCard post={post} size={BLOG_CARD_SIZE_OPTIONS.medium} />
+          ))
+        )}
+      </Grid>
     </Grid>
   );
 };
